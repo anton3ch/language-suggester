7abd70013701fce6d4f5b7b1679662060acc2c40
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-10-21 15:32:24 -0500

    add text shadows, change opacity and styling of  branched cards'

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,6 @@ function hideResults() {
   document.getElementById("csharp").setAttribute("class", "hidden");
 }
 
-
 window.addEventListener("load", function() {
   const form = document.querySelector("form");
 
@@ -16,56 +15,63 @@ window.addEventListener("load", function() {
     hideResults();
     let name = document.querySelector("input#inputName").value;
     let catDog = document.querySelector("input[name='catDog']:checked");
-    
-
-    
 
     if (name) {
       
       document.querySelector("span#insertName").innerText = name;
       document.getElementById("resultName").removeAttribute("class");
       let zodiacSelected = document.getElementById("zodiacSelect").value;
-      
       zodiacSelected = parseInt(zodiacSelected);
-      
+      let research = document.querySelector("input[name='research']:checked").value;
+      let experience = document.getElementById("experienceCheck").checked;
 
       if(zodiacSelected === 1 || zodiacSelected === 4 || zodiacSelected === 7 || zodiacSelected === 10){
 
-        let research = document.querySelector("input[name='research']:checked").value;
-        let experience = document.getElementById("experienceCheck").checked;
-
-        if(research === "no" || research === "meh"){
-          document.getElementById("html").removeAttribute("class");
-        } else if (research === "yes" && experience === false) {
+        if (research === "yes" && experience === false) {
           document.getElementById("css").removeAttribute("class");
           document.getElementById("html").removeAttribute("class");
         } else if (research === "yes" && experience === true) {
           document.getElementById("javascript").removeAttribute("class");
-        };
+        } else if(research === "no"){
+          document.getElementById("html").removeAttribute("class");
+        } else if(research === "meh"){
+          document.getElementById("css").removeAttribute("class");
+        }
 
       } else if(zodiacSelected === 2 || zodiacSelected === 5 || zodiacSelected === 8 || zodiacSelected === 11) {
         
-        if(research === "no" || research === "meh"){
+        if (research === "yes" && experience === false) {
+          document.getElementById("css").removeAttribute("class");
           document.getElementById("html").removeAttribute("class");
-        } else if (research === "yes") {
-          document.getElementById("javascript").removeAttribute("class");
-        };
+        } else if (research === "yes" && experience === true) {
+          document.getElementById("csharp").removeAttribute("class");
+        } else if(research === "no"){
+          document.getElementById("html").removeAttribute("class");
+        } else if(research === "meh"){
+          document.getElementById("css").removeAttribute("class");
+        }
+
 
       } else if(zodiacSelected === 3 || zodiacSelected === 6 || zodiacSelected === 9 || zodiacSelected === 12) {
         
-        if(research === "no" || research === "meh"){
+        if (research === "yes" && experience === false) {
+          document.getElementById("css").removeAttribute("class");
           document.getElementById("html").removeAttribute("class");
-        } else if (research === "yes") {
-          document.getElementById("javascript").removeAttribute("class");
-        };
+        } else if (research === "yes" && experience === true) {
+          document.getElementById("swift").removeAttribute("class");
+        } else if(research === "no"){
+          document.getElementById("html").removeAttribute("class");
+        } else if(research === "meh"){
+          document.getElementById("css").removeAttribute("class");
+        }
+      }
 
-      };
     } else {
       document.querySelector("h1").innerText = "Please, input your name!".toUpperCase();
-      document.querySelector("h1").style.color = "linear-gradient(166deg, rgba(232,85,85,1) 0%, rgba(61,46,150,1) 100%)";
-    };
-   
- 
+      document.querySelector("h1").style.color = "red";
+      document.querySelector("h1").style.fontSize = "4em";
+      document.querySelector("h1").style.textShadow = "2px 2px 2px #ffffff";
+    }
   });
 
   const selectedDog = document.getElementById("dogRadioLabel");
@@ -73,8 +79,8 @@ window.addEventListener("load", function() {
   
   dogHover.addEventListener("mouseover", function() {
       selectedDog.innerText = "Please, make a better choice!";
-  })
+  });
   dogHover.addEventListener("mouseout", function() {
     selectedDog.innerText = "Dogs";
-})
+  });
 });
