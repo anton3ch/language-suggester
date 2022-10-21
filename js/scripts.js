d0062c34dfaa56c5613ddb9e10e278e0f1c07479
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-10-21 12:52:28 -0500

    add error handling for name field

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,26 @@ window.addEventListener("load", function() {
     event.preventDefault();
     hideResults();
     const name = document.querySelector("input#inputName").value;
-    const zodiac = document.querySelector("input#zodiacSelect").value;
-    const catdog = document.querySelector("input[name='catdog']:checked").value;
-    const research = document.querySelector("input[name='research']:checked").value;
-    const experience = document.getElementById("experienceCheck").checked;
+    // const zodiac = document.querySelector("input#zodiacSelect").value;
+    // const catDog = document.querySelector("input[name='catDog']:checked").value;
+    // const research = document.querySelector("input[name='research']:checked").value;
+    // const experience = document.getElementById("experienceCheck").checked;
 
+    
 
+    if (name) {
+      document.querySelector("span#insertName").innerText = name;
+      document.getElementById("resultName").removeAttribute("class");
+
+      // if(){
+
+      // } else if() {
+
+      // } else if {}
+    } else {
+      document.querySelector("h1").innerText = "Please, input your name!".toUpperCase();
+      document.querySelector("h1").style.color = "linear-gradient(166deg, rgba(232,85,85,1) 0%, rgba(61,46,150,1) 100%)";
+    }
     
   });
 });
