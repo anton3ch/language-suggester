5c0fe24059272b341c671ece9bb2a8868110d46b
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-10-21 13:14:41 -0500

    add if statements for branches

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,8 @@ window.addEventListener("load", function() {
     event.preventDefault();
     hideResults();
     const name = document.querySelector("input#inputName").value;
-    // const zodiac = document.querySelector("input#zodiacSelect").value;
-    // const catDog = document.querySelector("input[name='catDog']:checked").value;
+    const zodiac = document.querySelector("input#zodiacSelect").value;
+    const catDog = document.querySelector("input[name='catDog']:checked");
     // const research = document.querySelector("input[name='research']:checked").value;
     // const experience = document.getElementById("experienceCheck").checked;
 
@@ -24,15 +24,24 @@ window.addEventListener("load", function() {
       document.querySelector("span#insertName").innerText = name;
       document.getElementById("resultName").removeAttribute("class");
 
-      // if(){
+      // if(zodiac === 1 || zodiac === 4 || zodiac === 7 || zodiac === 10){
 
-      // } else if() {
+      // } else if(zodiac === 2 || zodiac === 5 || zodiac === 8 || zodiac === 11) {
 
-      // } else if {}
+      // } else if(zodiac === 3 || zodiac === 6 || zodiac === 9 || zodiac === 12) {}
     } else {
       document.querySelector("h1").innerText = "Please, input your name!".toUpperCase();
       document.querySelector("h1").style.color = "linear-gradient(166deg, rgba(232,85,85,1) 0%, rgba(61,46,150,1) 100%)";
-    }
+    };
+    const selectedDog = document.getElementById("dogradioLabel");
+    selectedDog.addEventListener('click', function() {
+      if (catDog.value === 'dog') {
+        selectedDog.innerText = "please make a better choice";
+      } else {
+        
+      }
+    })
+    
     
   });
 });
